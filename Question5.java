5bb2cf620c9b530acd188b05bf550af6c38f943a
Kwongmingwei <64054152+Kwongmingwei@users.noreply.github.com>
2020-04-22 14:21:02 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Question5
 {
@@ -27,6 +29,36 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter number of integers:");
+    int num1=in.nextInt();
+    List<Integer>List1=new ArrayList<Integer>();
+    List<Integer>List2=new ArrayList<Integer>();
+    for (int i=0;i<num1;i++){
+      System.out.println("Enter integer:");
+      int numb=in.nextInt();
+      if (List1.contains(numb)){
+        int a=List1.indexOf(numb);
+        List2.set(a, List2.get(a)+1);
+      }
+      else{
+        List1.add(numb);
+        List2.add(1);
+      }
+    }
+    int c=0;
+    if (List2.size()<1){
+      System.out.println("Less than 1 integer");
+    }
+    else{
+      for (int i=0;i<List2.size();i++){
+        if(c<List2.get(i)){
+        c=List2.get(i);
+        }
+      }
+    }
+    int e=List2.indexOf(c);
+    int d=List1.get(e);
+    System.out.println("Mode is: "+d);
     
   }
 }
